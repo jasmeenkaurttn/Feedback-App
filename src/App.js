b7e2d8c992d0c74b6ad372d2cc55b4735c6247c1
jasmeenkaurttn <jasmeen.kaur@tothenew.com>
2022-03-03 12:35:45 +0530

    Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    
+    <>
+      <Header />
+      <div className='container'>
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
